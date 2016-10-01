298ae713943dc3628e349e8b8a0729e16bb6d887
Slava <randomnuy@gmail.com>
2016-10-01 13:04:26 +0000

    Java

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function sum (a, b, c) {
+    var result = a + b + c;
+    return result;
+}
+
+var num1 = Number (prompt('Vveditb rik narogenia'));
+var num2 = Number (prompt('Vveditb misyatsb narogenia'));
+var num3 = Number (prompt('Vveditb denb narogenia'));
+var result = sum(num1, num2, num3)
+document.writeln(result);
